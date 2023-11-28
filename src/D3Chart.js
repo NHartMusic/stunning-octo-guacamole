c19f1f2e75013185b47d3b8b54dceaac23efa7a5
nick_hart <nhartmusic@gmail.com>
2023-11-28 14:15:27 -0800

    bar chart coming together

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,22 +1,24 @@
 import * as d3 from 'd3'
 
 const url = 'https://udemy-react-d3.firebaseio.com/tallest_men.json'
+const Width = 800
+const Height = 500
 
 export default class D3Chart {
     constructor(element) {
         const svg = d3.select(element)
             .append('svg')
-            .attr('width', 800)
-            .attr('height', 800)
+            .attr('width', Width)
+            .attr('height', Height)
 
         d3.json(url).then(data=> {
             const y = d3.scaleLinear()
-                .domain([0, 272])
-                .range([0, 500])  
+                .domain([0, d3.max(data, d =>  d.height)])
+                .range([0, Height])  
                 
             const x = d3.scaleBand()
                 .domain(data.map(d => d.name))
-                .range([0, 800])
+                .range([0, Width])
                 .padding(0.4)
 
             const rects = svg.selectAll('rect')
@@ -24,7 +26,7 @@ export default class D3Chart {
 
             rects.enter().append('rect')
                 .attr('x', d => x(d.name))
-                .attr('y', 50)
+                .attr('y', d => Height - y(d.height))
                 .attr('width', x.bandwidth)
                 .attr('height', d => y(d.height))
                 .attr('fill', 'grey')
